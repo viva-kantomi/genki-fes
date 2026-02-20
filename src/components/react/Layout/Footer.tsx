bdb733561c2c8303b94d2f9f078d12b17f6e7c5b
GZ_ROCK <oraoraoraora7lgo@gmail.com>
2026-02-20 17:51:59 +0900

    修正：フッターとサイドバーのナビゲーションリンクテキストを日本語に変更し、2026 げんきフェスタの特設ページへのリンクを追加。これにより、ユーザーの利便性を向上させる。

--- src/components/react/Layout/Footer.tsx
+++ src/components/react/Layout/Footer.tsx
@@ -11,8 +11,9 @@ export function Footer() {
           <Link to="/">トップ</Link>
           <Link to="/events/">イベント詳細</Link>
           <Link to="/history/">これまでの活動</Link>
-          <Link to="/news/">NEWS</Link>
-          <Link to="/about/">ABOUT</Link>
+          <Link to="/genki-festa-2026/">2026 げんきフェスタ<br/>特設ページ</Link>
+          <Link to="/news/">お知らせ</Link>
+          <Link to="/about/">私たちについて</Link>
         </nav>
         <p className="copyright">&copy; 2026 げんき塾チーム</p>
         <p className="credits">icon by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a></p>
